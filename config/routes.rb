1ceb8c62db5fe620bbe986f3cb595fc4133a8d9c
Philip Bedward <UC214475@ten.thomsonreuters.com>
2016-10-19 16:53:48 -0400

    Added project root page; Added index and new actions for Task

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # get "/",:to => "/index"
+  get "/tasks" => "task#index"
+  get "/tasks/new" => "tasks#new" , as: :newTask
 end
