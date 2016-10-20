729280001df1181305cc1578d557909304d4a9f4
Philip Bedward <UC214475@ten.thomsonreuters.com>
2016-10-20 11:04:49 -0400

    added create action to save a new task when it's submitted

--- app/controllers/task_controller.rb
+++ app/controllers/task_controller.rb
@@ -8,5 +8,19 @@ class TaskController < ApplicationController
     @task = Task.new
   end
 
+  def create
+
+    if Task.create(task_params)
+      redirect_to action:"index"
+    else
+      render "new"
+    end
+
+  end
+
+  private
+    def task_params
+      params.require(:task).permit(:name)
+    end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   # get "/",:to => "/index"
   get "/tasks" => "task#index"
-  get "/tasks/new" => "tasks#new" , as: :newTask
+  get "/tasks/new" => "task#new" , as: :newTask
+  post "/tasks" => "task#create"
 end
