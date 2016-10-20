83c1a899dd3e53ec751416c2ee339f6ae85c964f
Philip Bedward <UC214475@ten.thomsonreuters.com>
2016-10-20 11:28:07 -0400

    added url to tasks on the index pg to start the 'show ' action

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,5 @@ Rails.application.routes.draw do
   get "/tasks" => "task#index"
   get "/tasks/new" => "task#new" , as: :newTask
   post "/tasks" => "task#create"
+  get "/tasks/:id" => "task#show", as: :task
 end
