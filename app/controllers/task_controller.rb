b9f02eafa9e26ec306a34a3d5c4c80ad0e0233f1
philipbed <philipbed813@gmail.com>
2016-10-20 11:28:07 -0400

    added url to tasks on the index pg to start the 'show ' action

--- app/controllers/task_controller.rb
+++ app/controllers/task_controller.rb
@@ -18,6 +18,10 @@ class TaskController < ApplicationController
 
   end
 
+  def show
+    @task = Task.find(params[:id])
+  end
+
   private
     def task_params
       params.require(:task).permit(:name)
